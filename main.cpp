5a97dd1671562f05774a19f70afd8bb1cdf4adaa
Peter Smrecek <petersmrecek@gmail.com>
2021-10-15 11:06:56 +0200

    Refactoring

--- main.cpp
+++ main.cpp
@@ -33,7 +33,6 @@ bool read_from_file(const char* fileName, std::vector<std::vector<int>>& allLine
 	std::ifstream ifs{ fileName };
 	if (!ifs.is_open())
 	{
-		std::cout << "Invalid file" << std::endl;
 		return false;
 	}
 
@@ -52,28 +51,10 @@ bool read_from_file(const char* fileName, std::vector<std::vector<int>>& allLine
 
 	if (ifs.eof())
 	{
-		//std::cout << "File read successful" << std::endl;
 		return true;
 	}
 
 	return false;
-}
-
-bool read_from_standard(std::vector<std::vector<int>>& allLines)
-{
-	std::string strLine;
-	std::getline(std::cin, strLine);
-
-	std::vector<int> oneLine;
-
-	if (!(check_valid_input_line(strLine, oneLine)))
-	{
-		return false;
-	}
-
-	allLines.push_back(oneLine);
-
-	return true;
 }
 
 bool is_in_row_or_col(const std::vector<std::vector<int>>& sudoku, int num, int row, int col)
@@ -290,27 +271,14 @@ int sudoku_solver(const std::vector<int>& oneLineFull)
 	return 0;
 }
 
-int main(int argc, char* argv[])
-//int main()
+int set_variables(int argc, char* argv[], bool& inputFile, bool& outputFile, const char*& inputFileName, const char*& outputFileName) 
 {
-	std::vector<std::vector<int>> allLines;
-	bool inputFile = false;
-	bool outputFile = false;
-	auto inputFileName = "";
-	auto outputFileName = "";
-
-	//std::cout << argc << std::endl;
-	//for (int i = 0; i < argc; i++)
-	//{
-	//	std::cout << argv[i] << " ";
-	//}
-	//std::cout << std::endl;
 
 	if (argc == 1)
 	{
 		inputFile = false;
 		outputFile = false;
-	} 
+	}
 	else if (argc == 3)
 	{
 		if (std::string(argv[1]) == "-i")
@@ -364,20 +332,65 @@ int main(int argc, char* argv[])
 	{
 		return 2;
 	}
+	
+	return 0;
+}
 
+bool read_from_standard()
+{
+	std::string strLine;
+	while (std::getline(std::cin, strLine))
+	{
+		std::vector<int> oneLine;
+
+		if (!(check_valid_input_line(strLine, oneLine)))
+		{
+			return false;
+		}
+
+		sudoku_solver(oneLine);
+	}
+
+	return true;
+}
+
+bool sudoku_solver_caller(bool inputFile, const char* inputFileName, std::vector<std::vector<int>>& allLines)
+{
 	if (inputFile)
 	{
 		if (!(read_from_file(inputFileName, allLines)))
 		{
-			return 2;
+			return false;
+		}
+
+		for (size_t i = 0; i < allLines.size(); i++)
+		{
+			sudoku_solver(allLines[i]);
 		}
 	}
 	else
 	{
-		if (!(read_from_standard(allLines)))
+		if (!(read_from_standard()))
 		{
-			return 2;
+			return false;
 		}
+	}
+
+	return true;
+}
+
+int main(int argc, char* argv[])
+//int main()
+{
+	std::vector<std::vector<int>> allLines;
+	bool inputFile = false;
+	bool outputFile = false;
+	const char* inputFileName = "";
+	const char* outputFileName = "";
+
+	if (set_variables(argc, argv, inputFile, outputFile, inputFileName, outputFileName) != 0)
+	{
+		return 2;
 	}
 
 	std::streambuf* coutbuf{ std::cout.rdbuf() };
@@ -390,96 +403,21 @@ int main(int argc, char* argv[])
 		}
 		std::cout.rdbuf(outs.rdbuf());
 
-		for (size_t i = 0; i < allLines.size(); i++)
+		if (!(sudoku_solver_caller(inputFile, inputFileName, allLines)))
 		{
-			sudoku_solver(allLines[i]);
+			return 2;
 		}
+
 		std::cout.rdbuf(coutbuf);
 		outs.close();
 	}
 	else
 	{
-		for (size_t i = 0; i < allLines.size(); i++)
+		if (!(sudoku_solver_caller(inputFile, inputFileName, allLines)))
 		{
-			sudoku_solver(allLines[i]);
+			return 2;
 		}
 	}
 
-
 	return 0;
 }
-
-
-//	if (std::string(argv[1]) != "-i")
-//	{
-//		std::cerr << "Invalid flag";
-//		return 1;
-//	}
-
-
-//	std::ifstream ifs{ "input.txt" };
-//	// Ma viacero stavov, treba si na to davat pozor
-//
-//	std::string line;
-//
-//	if (!ifs.is_open())
-//	{
-//		std::cout << "Invalid file" << std::endl;
-//		return 1;
-//	}
-//
-//	while (std::getline(ifs, line))
-//	{
-//		std::cout << line << std::endl;
-//		// Neriesit konce riadkov v subore vobec, nechat to na standardnu kniznicu
-//		// Ako koniec pouzivat idealne std::endl, ale da sa aj klasicke "\n"
-//
-//	}
-//
-//	if (ifs.eof())
-//	{
-//		std::cout << "File read successful" << std::endl;
-//		return 0;
-//	}
-
-
-
-
-
-//std::vector<int> vec1;
-//std::vector<int> vec2;
-//std::vector<int> vec3;
-//std::vector<std::vector<int>> vecvec;
-//
-//for (int i = 0; i < 10; i++)
-//{
-//	vec1.push_back(i + 10);
-//	vec2.push_back(i + 100);
-//	vec3.push_back(i + 1000);
-//}
-//
-//for (int i = 0; i < 10; i++)
-//{
-//	vec2.push_back(i + 1000);
-//	vec3.push_back(i + 10000);
-//}
-//
-//for (int i = 0; i < 10; i++)
-//{
-//	vec3.push_back(i + 100000);
-//}
-//
-//vec1.push_back('a');
-//
-//vecvec.push_back(vec1);
-//vecvec.push_back(vec2);
-//vecvec.push_back(vec3);
-//
-//for (size_t i = 0; i < vecvec.size(); i++)
-//{
-//	std::cout << std::endl << vecvec[i].size() << ": ";
-//	for (size_t j = 0; j < vecvec[i].size(); j++)
-//	{
-//		std::cout << vecvec[i][j] << " ";
-//	}
-//}
